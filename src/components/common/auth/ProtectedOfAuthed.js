2d9fca5cc31a8b6a0475f6af4058d86a6187073d
Dariush <paeezymohammad@gmail.com>
2022-01-31 11:55:50 +0330

    add_useEffect

--- src/components/common/auth/ProtectedOfAuthed.js
+++ src/components/common/auth/ProtectedOfAuthed.js
@@ -1,12 +1,22 @@
-import React from "react";
-import { Redirect, Route } from "react-router-dom";
-import Panel from "components/common/panel/Panel";
-import { useCookies } from "react-cookie";
+import React, { useState, useEffect } from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import Panel from 'components/common/panel/Panel';
+import { useCookies } from 'react-cookie';
 
 const ProtectedOfAuthed = (props) => {
-    const [cookies] = useCookies(["token"]);
+    const [cookies] = useCookies(['token']);
+    const [returnValue, setReturnValue] = useState(null);
 
-    return !cookies['token'] ? <Route {...props} /> : <Redirect to="/dashboard" />;
+    useEffect(() => {
+        if (!cookies['token']) {
+            setReturnValue(<Route {...props} />);
+        } else {
+            setReturnValue(<Redirect to="/dashboard" />);
+        }
+    }, [cookies]);
+
+    // return !cookies['token'] ? <Route {...props} /> : alert(cookies['token']);
+    return returnValue;
 };
 
 export default ProtectedOfAuthed;
