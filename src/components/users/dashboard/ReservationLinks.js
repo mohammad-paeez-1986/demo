8a7cc57a0c3c65fdf73bdef3bce39ba4f36d7d4a
Dariush <paeezymohammad@gmail.com>
2022-02-02 21:38:25 +0330

    responsive_user_dashboard

--- src/components/users/dashboard/ReservationLinks.js
+++ src/components/users/dashboard/ReservationLinks.js
@@ -1,39 +1,44 @@
-import React from "react";
-import { Card, Col, List, Tooltip } from "antd";
-import { Link } from "react-router-dom";
-import gym from "images/gym.png";
-import carwash from "images/carwash.png";
-import cafe from "images/cafe.png";
-import saloon from "images/saloon.png";
-import { getWelfareName } from "general/Helper";
+import React from 'react';
+import { Card, Col, List, Tooltip } from 'antd';
+import { Link } from 'react-router-dom';
+import gym from 'images/gym.png';
+import carwash from 'images/carwash.png';
+import cafe from 'images/cafe.png';
+import saloon from 'images/saloon.png';
+import { getWelfareName } from 'general/Helper';
 
 const ReservationLinks = () => {
     const data = [
         {
-            link: "cafe/reservation",
+            link: 'cafe/reservation',
             image: cafe,
-            name: "کافه",
+            name: 'کافه',
         },
         {
-            link: "carwash/reservation",
+            link: 'carwash/reservation',
             image: carwash,
-            name: "کارواش",
+            name: 'کارواش',
         },
         {
-            link: "saloon/reservation",
+            link: 'saloon/reservation',
             image: saloon,
-            name: "اتاق جلسات",
+            name: 'اتاق جلسات',
         },
         {
-            link: "gym/reservation",
+            link: 'gym/reservation',
             image: gym,
-            name: "باشگاه ورزشی",
+            name: 'باشگاه ورزشی',
         },
     ];
 
     return (
         <List
-            grid={{ gutter: 16, column: 4 }}
+            grid={{
+                gutter: 16,
+                column: 4,
+                xs: 1,
+                sm: 2,
+            }}
             dataSource={data}
             renderItem={({ link, image, name }) => (
                 <List.Item>
