769beb5acb12fde9c60ea2e492776144f9dceb53
Dariush <paeezymohammad@gmail.com>
2022-02-04 18:48:07 +0330

    show_today_reserves_automatically

--- src/components/admin/program/AddDailyProgram.js
+++ src/components/admin/program/AddDailyProgram.js
@@ -1,8 +1,8 @@
-import { useState, useEffect } from "react";
-import persian from "react-date-object/calendars/persian";
-import persian_fa from "react-date-object/locales/persian_fa";
-import AddDailyProgramModal from "./AddDailyProgramModal";
-import { Calendar, DateObject } from "react-multi-date-picker";
+import { useState, useEffect } from 'react';
+import persian from 'react-date-object/calendars/persian';
+import persian_fa from 'react-date-object/locales/persian_fa';
+import AddDailyProgramModal from './AddDailyProgramModal';
+import { Calendar, DateObject } from 'react-multi-date-picker';
 import {
     // Card,
     // Col,
@@ -13,17 +13,17 @@ import {
     Table,
     Spin,
     Popconfirm,
-} from "antd";
-import notify from "general/notify";
-import axios from "axios";
-import { getDateFromObject } from "general/Helper";
+} from 'antd';
+import notify from 'general/notify';
+import axios from 'axios';
+import { getDateFromObject } from 'general/Helper';
 import {
     CloseOutlined,
     CheckOutlined,
     DeleteOutlined,
-} from "@ant-design/icons";
+} from '@ant-design/icons';
 
-const weekDays = ["ش", "ی", "د", "س", "چ", "پ", "ج"];
+const weekDays = ['ش', 'ی', 'د', 'س', 'چ', 'پ', 'ج'];
 // const weekDays = ["شنبه", "یکشنبه", "دوشنبه", "سه شنبه", "چهارشنبه", "پنجشنبه", "جمعه"]
 
 const AddDailyProgram = ({ match }) => {
@@ -33,9 +33,9 @@ const AddDailyProgram = ({ match }) => {
     const [selectedDayProgramList, setSelectedDayProgramList] = useState([]);
     const [welfareId, setWelfareId] = useState(null);
     const [copyDaysList, setCopyDaysList] = useState([]);
-    const [copyBoxVisible, setCopyBoxVisible] = useState("hide");
+    const [copyBoxVisible, setCopyBoxVisible] = useState('hide');
     const [copyButtonDisabled, setCopyButtonDisabled] = useState(true);
-    const [doCopyButtonVisible, setDoCopyButtonVisible] = useState("hide");
+    const [doCopyButtonVisible, setDoCopyButtonVisible] = useState('hide');
     const [isSelectingDays, setIsSelectingDays] = useState(false);
     const [holidaysList, setHolidaysList] = useState([]);
     const { url } = match;
@@ -43,8 +43,8 @@ const AddDailyProgram = ({ match }) => {
     const resetCopyBox = () => {
         setDate(null);
         setCopyDaysList([]);
-        setDoCopyButtonVisible("hide");
-        setCopyBoxVisible("hide");
+        setDoCopyButtonVisible('hide');
+        setCopyBoxVisible('hide');
         setCopyButtonDisabled(true);
         setIsSelectingDays(false);
         setSelectedDayProgramList([]);
@@ -53,21 +53,23 @@ const AddDailyProgram = ({ match }) => {
 
     useEffect(() => {
         resetCopyBox();
-        const type = url.split("/")[1]?.toUpperCase();
-        axios.post("Welfare/Get", { type }).then(({ data }) => {
+        const today = new DateObject({ calendar: persian, locale: persian_fa });
+        const type = url.split('/')[1]?.toUpperCase();
+        axios.post('Welfare/Get', { type }).then(({ data }) => {
             // console.log(data);
             setWelfareId(data[0].id);
+            onDayChange(today, false, data[0].id);
         });
 
         axios
-            .post("Calendar/Get", { shamsiDate: "0", description: "all" })
+            .post('Calendar/Get', { shamsiDate: '0', description: 'all' })
             .then(({ data }) => {
                 const holidaysList = data.map((item) => item.shamsiDate);
                 setHolidaysList(holidaysList);
             });
     }, [url]);
 
-    const onDayChange = (date, isObject = false) => {
+    const onDayChange = (date, isObject = false, welfareId) => {
         setSelectedDayProgramList([]);
         let selectedDate;
         if (!isObject) {
@@ -84,7 +86,7 @@ const AddDailyProgram = ({ match }) => {
 
         setLoading(true);
         axios
-            .post("Reservation/GetReservable", {
+            .post('Reservation/GetReservable', {
                 dateFrom: selectedDate,
                 welfareId,
             })
@@ -110,34 +112,34 @@ const AddDailyProgram = ({ match }) => {
         const today = new DateObject({ calendar: persian });
 
         if (copyDaysList.length >= 10) {
-            notify.warning("حداکثر ده مورد برای کپی مجاز است");
+            notify.warning('حداکثر ده مورد برای کپی مجاز است');
             return;
         }
 
         if (copyDaysList.length > 0) {
-            setDoCopyButtonVisible("show");
+            setDoCopyButtonVisible('show');
         }
 
         if (addingDate) {
             if (copyDaysList.length > 0) {
                 if (addingDate < today.format()) {
-                    notify.error("روزهای گذشته قابل انتخاب نیست");
+                    notify.error('روزهای گذشته قابل انتخاب نیست');
                     return;
                 }
             }
             setCopyButtonDisabled(true);
             setIsSelectingDays(true);
-            setCopyBoxVisible("show");
+            setCopyBoxVisible('show');
             setCopyDaysList([...new Set([...copyDaysList, addingDate])]);
         }
     };
 
     const cancelCopyDays = () => {
         setDate(copyDaysList[0]);
-        setCopyBoxVisible("hide");
+        setCopyBoxVisible('hide');
         setCopyButtonDisabled(false);
         setIsSelectingDays(false);
-        setDoCopyButtonVisible("hide");
+        setDoCopyButtonVisible('hide');
         setCopyDaysList([]);
     };
 
@@ -149,7 +151,7 @@ const AddDailyProgram = ({ match }) => {
 
         setLoading(true);
         axios
-            .post("DailyProgram/CopyDaily", values)
+            .post('DailyProgram/CopyDaily', values)
             .then(({ message }) => {
                 notify.success(message);
                 resetCopyBox();
@@ -161,7 +163,7 @@ const AddDailyProgram = ({ match }) => {
     const onDeleteProgram = (id) => {
         setLoading(true);
         axios
-            .post("DailyProgram/Remove", {
+            .post('DailyProgram/Remove', {
                 programId: id,
             })
             .then(() => {
@@ -178,13 +180,13 @@ const AddDailyProgram = ({ match }) => {
 
     const columns = [
         {
-            title: "گنجایش",
-            dataIndex: "capacity",
-            key: "capacity",
+            title: 'گنجایش',
+            dataIndex: 'capacity',
+            key: 'capacity',
         },
         {
-            title: "وی آی پی",
-            key: "isVip",
+            title: 'وی آی پی',
+            key: 'isVip',
             // className: "",
             render: (record) =>
                 record.isVip ? (
@@ -194,18 +196,18 @@ const AddDailyProgram = ({ match }) => {
                 ),
         },
         {
-            title: "زمان سانس",
-            key: "id",
+            title: 'زمان سانس',
+            key: 'id',
             render: ({ startTime, endTime }) => `${endTime} - ${startTime}`,
         },
         {
-            title: "جنیست",
-            dataIndex: "gender",
-            key: "gender",
+            title: 'جنیست',
+            dataIndex: 'gender',
+            key: 'gender',
         },
         {
-            title: "حذف",
-            className: "act-icon delete",
+            title: 'حذف',
+            className: 'act-icon delete',
             onCell: (record) => {
                 // console.log(record.id);
             },
@@ -222,89 +224,81 @@ const AddDailyProgram = ({ match }) => {
     return (
         <>
             {/* <Col sm={24} xs={24} md={19} lg={15} xlg={12}> */}
-                <Spin delay={900} spinning={loading}>
-                    {/* <Card title="ثبت برنامه جدید"> */}
-                        <div id="large-calendar">
-                            <Calendar
-                                calendar={persian}
-                                locale={persian_fa}
-                                calendarPosition="bottom-right"
-                                weekDays={weekDays}
-                                onChange={onDayChange}
-                                value={date}
-                                format={"YYYY/MM/DD"}
-                                mapDays={({ date }) => {
-                                    let props = {};
+            <Spin delay={900} spinning={loading}>
+                {/* <Card title="ثبت برنامه جدید"> */}
+                <div id="large-calendar">
+                    <Calendar
+                        calendar={persian}
+                        locale={persian_fa}
+                        calendarPosition="bottom-right"
+                        weekDays={weekDays}
+                        onChange={onDayChange}
+                        value={date}
+                        format={'YYYY/MM/DD'}
+                        mapDays={({ date }) => {
+                            let props = {};
 
-                                    if (
-                                        holidaysList.includes(
-                                            getDateFromObject(date)
-                                        )
-                                    )
-                                        return {
-                                            disabled: true,
-                                            style: { color: "#f30" },
-                                            onClick: () =>
-                                                notify.error(
-                                                    "این روز تعطیل می  باشد"
-                                                ),
-                                        };
-                                }}
-                            />
-                        </div>
-                        <br />
-                        <Table
-                            bordered
-                            columns={columns}
-                            pagination={{
-                                hideOnSinglePage: true,
-                            }}
-                            dataSource={selectedDayProgramList}
-                            loading={loading}
-                            scroll={{ x: true }}
-                        />
-                        <div className="ant-card-footer">
-                            <Space>
-                                <Button
-                                    onClick={() => setIsModalVisible(true)}
-                                    type="primary"
-                                    htmlType="submit"
-                                    className="wide-button"
-                                >
-                                    افزودن برنامه
-                                </Button>
-
-                                <Button
-                                    type="primary"
-                                    htmlType="submit"
-                                    className="wide-button"
-                                    onClick={copyProgram}
-                                    disabled={copyButtonDisabled}
-                                >
-                                    {!isSelectingDays
-                                        ? "کپی برنامه"
-                                        : "درحال انتخاب"}
-                                </Button>
-                            </Space>
-                        </div>
-                    {/* </Card> */}
-                </Spin>
-
-                <Modal
-                    title="افزودن برنامه"
-                    visible={isModalVisible}
-                    footer={null}
-                    destroyOnClose={true}
-                    onCancel={closeModal}
-                >
-                    <AddDailyProgramModal
-                        date={date}
-                        onSetNewProgram={onDayChange}
-                        closeModal={closeModal}
-                        welfareId={welfareId}
+                            if (holidaysList.includes(getDateFromObject(date)))
+                                return {
+                                    disabled: true,
+                                    style: { color: '#f30' },
+                                    onClick: () =>
+                                        notify.error('این روز تعطیل می  باشد'),
+                                };
+                        }}
                     />
-                </Modal>
-             {/* </Col> */}
+                </div>
+                <br />
+                <Table
+                    bordered
+                    columns={columns}
+                    pagination={{
+                        hideOnSinglePage: true,
+                    }}
+                    dataSource={selectedDayProgramList}
+                    loading={loading}
+                    scroll={{ x: true }}
+                />
+                <div className="ant-card-footer">
+                    <Space>
+                        <Button
+                            onClick={() => setIsModalVisible(true)}
+                            type="primary"
+                            htmlType="submit"
+                            className="wide-button"
+                        >
+                            افزودن برنامه
+                        </Button>
+
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            className="wide-button"
+                            onClick={copyProgram}
+                            disabled={copyButtonDisabled}
+                        >
+                            {!isSelectingDays ? 'کپی برنامه' : 'درحال انتخاب'}
+                        </Button>
+                    </Space>
+                </div>
+                {/* </Card> */}
+            </Spin>
+
+            <Modal
+                title="افزودن برنامه"
+                visible={isModalVisible}
+                footer={null}
+                destroyOnClose={true}
+                onCancel={closeModal}
+            >
+                <AddDailyProgramModal
+                    date={date}
+                    onSetNewProgram={onDayChange}
+                    closeModal={closeModal}
+                    welfareId={welfareId}
+                />
+            </Modal>
+            {/* </Col> */}
             <ul className={`copy-days-box ${copyBoxVisible}`}>
                 {copyDaysList.map((item) => {
                     return <li>{item}</li>;
